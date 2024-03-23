a6d2e843bc0b11c2601fc254aaa4e9671fbb83ec
neon-sunset <neon_sunset@outlook.com>
2024-03-23 14:20:15 +0200

    .

---
+++ RedisU16/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Concurrent;
+using System.Net.Sockets;
+
+var listener = new TcpListener(System.Net.IPAddress.Any, 6379);
+listener.Start();
+var redisClone = new RedisClone();
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var _ = redisClone.HandleConnection(client); // run async
+}
+
+public class RedisClone
+{
+    readonly ConcurrentDictionary<string, string> _state = new();
+
+    public async Task HandleConnection(TcpClient client)
+    {
+        using var _ = client;
+        using var stream = client.GetStream();
+        using var reader = new StreamReader(stream);
+        using var writer = new StreamWriter(stream)
+        {
+            NewLine = "\r\n"
+        };
+
+        try
+        {
+            var args = new List<string>();
+            while (true)
+            {
+                args.Clear();
+                var line = await reader.ReadLineAsync();
+                if (line == null) break;
+
+                if (line[0] != '*')
+                    throw new InvalidDataException("Cannot understand arg batch: " + line);
+
+                var argsv = int.Parse(line.Substring(1));
+                for (int i = 0; i < argsv; i++)
+                {
+                    line = await reader.ReadLineAsync();
+                    if (line == null || line[0] != '$')
+                        throw new InvalidDataException("Cannot understand arg length: " + line);
+                    var argLen = int.Parse(line.Substring(1));
+                    line = await reader.ReadLineAsync();
+                    if (line == null || line.Length != argLen)
+                        throw new InvalidDataException("Wrong arg length expected " + argLen + " got: " + line);
+
+                    args.Add(line);
+                }
+                var reply = ExecuteCommand(args);
+                if(reply == null)
+                {
+                    await writer.WriteLineAsync("$-1");
+                }
+                else
+                {
+                    await writer.WriteLineAsync($"${reply.Length}\r\n{reply}");
+                }
+                await writer.FlushAsync();
+            }
+        }
+        catch (Exception e)
+        {
+            try
+            {
+                string? line;
+                var errReader = new StringReader(e.ToString());
+                while ((line = errReader.ReadLine()) != null)
+                {
+                    await writer.WriteAsync("-");
+                    await writer.WriteLineAsync(line);
+                }
+                await writer.FlushAsync();
+            }
+            catch (Exception)
+            {
+                // nothing we can do
+            }
+        }
+    }
+
+    string? ExecuteCommand(List<string> args)
+    {
+        switch (args[0])
+        {
+            case "GET":
+                return _state.GetValueOrDefault(args[1]);
+            case "SET":
+                _state[args[1]] = args[2];
+                return null;
+            default:
+                throw new ArgumentOutOfRangeException("Unknown command: " + args[0]);
+        }
+    }
+}
---
+++ RedisU8/Program.cs
@@ -0,0 +1,98 @@
+﻿using NonBlocking;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Sockets;
+
+using var listener = new TcpListener(System.Net.IPAddress.Any, 6379);
+listener.Start();
+var redisClone = new RedisClone();
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var thread = new Thread(() => redisClone.HandleConnection(client))
+    {
+        Priority = ThreadPriority.AboveNormal
+    };
+
+    thread.Start();
+}
+
+public class RedisClone
+{
+    readonly ConcurrentDictionary<U8String, U8String> _state = [];
+
+    public void HandleConnection(TcpClient client)
+    {
+        using var _ = client;
+        using var socket = client.Client;
+        using var reader = socket.AsU8Reader(disposeSource: false);
+
+        try
+        {
+            var args = new List<U8String>();;
+            while (true)
+            {
+                args.Clear();
+                var lineRead = reader.ReadLine();
+                if (lineRead is not U8String line) break;
+                if (!line.StartsWith('*')) FormatException();
+
+                var argsv = int.Parse(line.Slice(1));
+                for (int i = 0; i < argsv; i++)
+                {
+                    line = reader.ReadLine() ?? [];
+
+                    if (!line.StartsWith('$')) FormatException();
+                    var argLen = int.Parse(line.Slice(1));
+
+                    line = reader.ReadLine() ?? [];
+                    if (line.Length != argLen) FormatException();
+                    args.Add(line);
+                }
+                var reply = ExecuteCommand(args);
+                if (reply == null)
+                {
+                    socket.Send(u8("$-1\r\n"));
+                }
+                else
+                {
+                    socket.Send($"${reply.Value.Length}\r\n{reply}\r\n");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            try
+            {
+                foreach (var line in u8(e.ToString()).Lines)
+                {
+                    socket.Send($"-{line}\r\n");
+                }
+            }
+            catch (Exception)
+            {
+                // nothing we can do
+            }
+        }
+    }
+
+    U8String? ExecuteCommand(List<U8String> args)
+    {
+        var cmd = args[0];
+        if (cmd == "GET"u8)
+            return _state.TryGetValue(args[1], out var value)
+                ? value : null;
+        if (cmd == "SET"u8)
+            _state[args[1]] = args[2];
+        else
+            FormatException();
+
+        return null;
+    }
+
+    [DoesNotReturn]
+    static void FormatException()
+    {
+        throw new FormatException();
+    }
+}
